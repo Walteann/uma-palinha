4e4edd4b2107c522ce02d923a1de3da6cea5ae34
Walteann <walteann3@gmail.com>
2021-11-29 20:45:48 -0300

    feat: scrollbar e btn toggle

--- src/pages/listener/components/section/components/header/Header.jsx
+++ src/pages/listener/components/section/components/header/Header.jsx
@@ -2,16 +2,13 @@ import "./Header.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { ToggleTheme } from "./components/toggle-theme/ToggleTheme";
 
 export const Header = () => (
     <header>
         <FontAwesomeIcon icon={faChevronLeft} className="btn" size="2x"/>
         <div className="header-icons-right">
-            <div>
-                {/* TODO: Colocara qui dps o Modo Dark */}
-                <label>Modo Dark</label>
-                <input type="checkbox" />
-            </div>
+            <ToggleTheme />
             <FontAwesomeIcon icon={faLinkedin} className="btn" size="2x"/>
             <FontAwesomeIcon icon={faGithub}  className="btn" size="2x"/>
         </div>
---
+++ src/pages/listener/components/section/components/header/components/toggle-theme/ToggleTheme.jsx
@@ -0,0 +1,7 @@
+import './ToggleTheme.scss';
+export const ToggleTheme = () => (
+        
+    <div className="wrapper" >
+        <input type="checkbox" />
+    </div>
+);
