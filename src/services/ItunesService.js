37a74d36158d96e48197b24d2976d296ca61addf
Walteann <walteann3@gmail.com>
2021-12-01 20:12:06 -0300

    refactor: other endpoint api to proxy

--- src/services/ItunesService.js
+++ src/services/ItunesService.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API_URL = 'https://itunes.apple.com';
+const API_URL = 'https://uma-palinha-proxy.herokuapp.com';
 
 export class ItunesService {
 
     static searchMusicByTerm(params) {
         params.limit = 20;
-        return axios.get(API_URL + '/search', {params})
+        return axios.get(`${API_URL}/api/v1/musics`, {params})
     }
 
 }
